8be4b37cee9557ecd56fc6662b47ce51cd0d562a
Felix Atoma <felixatoma2@gmail.com>
2024-06-10 10:54:10 +0000

    first commit

---
+++ js/index.js
@@ -0,0 +1,11 @@
+
+
+const btn = document.getElementById('btn-btn')
+btn.onclick= function(){
+  alert('Afaire a suivre') 
+
+}
+const prix = document.getElementById('prix-btn')
+btn.onclick= function(){
+    alert('suiver')
+}
